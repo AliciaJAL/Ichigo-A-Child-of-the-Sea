b0fb7805821e98679c317c2d02909b4f26e55d43
AliciaJAL <93489683+AliciaJAL@users.noreply.github.com>
2025-03-10 18:49:15 -0700

    experiment with physics

--- src/prefabs/Wave.js
+++ src/prefabs/Wave.js
@@ -3,7 +3,7 @@ class Wave extends PhysicsObject {
 		super(scene, x, y, texture, group) // call Sprite parent class
 
 		this.setOrigin(1, 1)
-		this.setScale(0.3)
+		this.setDisplaySize(window.innerWidth*(2833/2015))/5, (window.innerHeight*(2833/2015)/5)
 
 		// this.scene.physics.world.enable(this)
 		// Set Physics Body 
@@ -17,5 +17,6 @@ class Wave extends PhysicsObject {
 
 	update() {
 		this.setVelocityX(500)
+		console.log(this.x,this.y)
 	}
 }
